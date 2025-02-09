03a8ef21864986dfb77ddfd132fd8affe542c58c
Lujiale Guo <116628976+Guolujiale@users.noreply.github.com>
2025-02-10 04:32:09 +0800

    Add files via upload

---
+++ readdata.R
@@ -0,0 +1,4 @@
+# 读取 .rds 文件并打印数据
+data <- readRDS("data/merged_all.rds")
+print(data)
+
---
+++ readdata.py
@@ -0,0 +1,8 @@
+
+import pyreadr
+
+# 读取 .rds 文件
+result = pyreadr.read_r('data\merged_all.rds')
+
+# 查看结果
+print(result)
